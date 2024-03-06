099f80590b792f8a47b9d4f31263e98368769d94
Colleen Quintin <162430475+schmalkenti@users.noreply.github.com>
2024-03-06 11:08:13 +0200

    Create mainfile.js

---
+++ mainfile.js
@@ -0,0 +1,28 @@
+	if(!this.active){
+			this.active = true;
+			if(!this.external){
+				this.table.element.appendChild(this.getElement());
+				this.table.element.style.display = '';
+			}
+
+if(typeof this.table[name] === "undefined"){
+			this.table[name] = (...args) => {
+				this.table.initGuard(name);
+				
+				return func(...args);
+			};
+		}else{
+			console.warn("Unable to bind table function, name already in use", name);
+		}
+	}
+
+		if(this._handler){
+			lookupIndex = this.table.rowManager.displayPipeline.findIndex((item) => {
+				return item.handler === this._handler;
+			});
+
+			if(lookupIndex > -1){
+				index = lookupIndex;
+			}
+		}
+		
